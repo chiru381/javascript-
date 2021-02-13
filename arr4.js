e44bb488f4425de7b94410d2684d3b258725159a
chiru <chirukosanam123@gmail.com>
2021-02-13 19:17:12 +0530

    wtsapp

---
+++ arr4.js
@@ -0,0 +1,7 @@
+let arr = [4,4];
+for(x of arr){
+    console.log(x);
+}
+for(x of arr.keys()){
+    console.log(x);
+}
---
+++ arr5.js
@@ -0,0 +1,9 @@
+let arr = [9,3,5,6,3,4];
+for(var x=0; x<arr.length; x++){
+    if(arr[x] == 3){
+        console.log(arr[x]);
+        break;
+        continue;
+    }
+    continue;
+}
